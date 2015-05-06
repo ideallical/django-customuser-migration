1cac5d8c1ed2b41f7794e61b9f629234074f9f55
Michael van de Waeter <mvdwaeter@gmail.com>
2015-05-06 17:34:06 +0200

    Changed gis model to normal model

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-from django.contrib.gis.db import models
+from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 
